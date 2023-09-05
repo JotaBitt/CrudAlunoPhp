e6123a210c77fa57ecd6348fe2a634c0e1521990
João Pedro <101153871+JotaBitt@users.noreply.github.com>
2023-09-05 14:40:28 -0300

    listar Aluno concluído
    Listar aluno lendo um arquivo!! com php

--- listarAluno.php
+++ listarAluno.php
@@ -1,64 +1,53 @@
-<?php
-        $matricula = "";
-		$periodo = "";
-		$nome = "";
-        $email = ""
+<?php 
+    $nome = "";
+    $email = "";
+    $matricula = "";
+    $periodo = "";
 
-        $arquivoAluno = fopen("alunos.txt", "r") or die("erro ao criar arquivo");
-        $x = 0;
-		$linhas[] = fgets($arquivoAluno);
+    $arquivoAluno = fopen("alunos.txt", "r") or die ("Erro ao abrir arquivo");
 
+    $x = 0;
+    $linhas[] = fgets($arquivoAluno);
 ?>
+
 <!DOCTYPE html>
-<html>
-<head>
-     <style>
-        table, th, td {
-            border: 1px solid black;
-            border-collapse: collapse;
-        }
-    </style>
-</head>
-<body>
-    <h1>Lista alunos:</h1>
-<table>
-<tr>
-    <th>Nome</th>
-    <th>Email<th>
-    <th>Matrícula<th>
-    <th>Período<th>
-</tr>
+<html lang="pt-br">
+    <head>
+        <style>
+            table, th, td {
+                border: 1px solid black;
+                border-collapse: collapse;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Alunos:</h1>
+        <table style = "width:30%">
+        <tr>
+            <th>Nome</th>
+            <th>Idade</th>
+            <th>Matrícula</th>
+            <th>Email</th>
+        </tr>
 <?php
-    $x=0;
     while (!feof($arquivoAluno)) {
-			$linhas[] = fgets($arquivoAluno);
-			$colunaDados = explode(";", $linhas[$x]);
-			$nome = $colunaDados[0];
-			$email = $colunaDados[1];
-			$matricula = $colunaDados[2];
-            $periodo = $colunadados[3];
-            echo "<tr>";
-            echo "<td>" . $nome . "</td>";
-            echo "<td>" . $email . "</td>";
-            echo "<td>" . $matricula . "</td>";
-            echo "<td>" . $periodo . "</td>";
-            echo "</tr>";
-			
-    }
-    fclose($arquivoAluno);
+        $linhas[] = fgets($arquivoAluno);
+        $dados = explode(";", $linhas[$x]);
+        $nome = $dados[0];
+        $email = $dados[1];
+        $matricula = $dados[2];
+        $periodo = $dados[3];
 ?>
-</table>
-    <tr style ="text-align: center">
-    <td><?php echo $nome ?></td>
-    <td><?php echo $email ?></td>
-    <td><?php echo $matricula ?></td>
-    <td><?php echo $periodo ?></td>
-    <?php $x++;
- }
- 
+
+<tr style="text-align: center;">
+    <td><?php echo $nome?></td>
+    <td><?php echo $email?></td>
+    <td><?php echo $matricula?></td>
+    <td><?php echo $periodo?></td>
+</tr>
+
+<?php $x++;
+}
 ?>
 </tr>
-<p><?php echo $msg ?></p>
-<br>
-</body>
-</html>
+</table>
